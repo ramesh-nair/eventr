145cc5ab228ee769c36b3a269d84a76dc2d476a7
DGalstyan <galdav85@gmail.cim>
2018-10-28 16:24:17 +0400

    Default Changelist

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 require 'redis-namespace'
-redis_connection = Redis.new(:host => "localhost", :port => "6379", :thread_safe => true)
+redis_connection = Redis.new(:host => "127.0.0.1", :port => "6379", :thread_safe => true)
 Redis.current = Redis::Namespace.new(:eventr, :redis => redis_connection)
 
 $eventr_redis = Redis::Namespace.new(:eventr_app, :redis => redis_connection)
